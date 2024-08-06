257e96bba45dcdd96f37505e6c016f733643a5c8
Yulia Mishieva <kotyasher@gmail.com>
2024-08-06 21:03:58 +0300

    add webpack

---
+++ Makefile
@@ -0,0 +1,13 @@
+install:
+	npm ci
+
+lint:
+	npx eslint .
+
+start:
+	npx webpack serve --open
+
+build:
+	rm -rf dist
+	NODE_ENV=production npx webpack
+
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+
+const isProduction = process.env.NODE_ENV === "production";
+const stylesHandler = isProduction
+  ? MiniCssExtractPlugin.loader
+  : "style-loader";
+
+const config = {
+  mode: process.env.NODE_ENV || "development",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "index.html",
+    }),
+  ].concat(isProduction ? [new MiniCssExtractPlugin()] : []),
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/i,
+        use: [stylesHandler, "css-loader", "postcss-loader", "sass-loader"],
+      },
+    ],
+  },
+};
+
+export default config;
