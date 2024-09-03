b7e83fef65f7f9d6c8388a3e96e9925b2a354b76
Yulia Mishieva <kotyasher@gmail.com>
2024-09-03 13:47:50 +0300

    fix lint: singlequote quotes 2

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,23 @@
-import HtmlWebpackPlugin from "html-webpack-plugin";
-import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 
-const isProduction = process.env.NODE_ENV === "production";
+const isProduction = process.env.NODE_ENV === 'production';
 const stylesHandler = isProduction
   ? MiniCssExtractPlugin.loader
-  : "style-loader";
+  : 'style-loader';
 
 const config = {
-  mode: process.env.NODE_ENV || "development",
+  mode: process.env.NODE_ENV || 'development',
   plugins: [
     new HtmlWebpackPlugin({
-      template: "index.html",
+      template: 'index.html',
     }),
   ].concat(isProduction ? [new MiniCssExtractPlugin()] : []),
   module: {
     rules: [
       {
         test: /\.(sa|sc|c)ss$/i,
-        use: [stylesHandler, "css-loader", "postcss-loader", "sass-loader"],
+        use: [stylesHandler, 'css-loader', 'postcss-loader', 'sass-loader'],
       },
     ],
   },
